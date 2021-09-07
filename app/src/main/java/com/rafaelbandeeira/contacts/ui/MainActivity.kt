f93885997229c14ada27906490210cf4a536723e
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-09-07 03:05:53 -0300

    First commit

---
+++ app/src/main/java/com/rafaelbandeeira/contacts/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rafaelbandeeira.contacts.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rafaelbandeeira.contacts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
